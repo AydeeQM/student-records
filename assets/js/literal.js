59a315ec2840201bd66a69aa4fcb313ba5262326
aydee <aydee.qm@gmail.com>
2017-09-20 17:07:22 -0500

    diseñando declarativo

--- assets/js/literal.js
+++ assets/js/literal.js
@@ -1,3 +1,10 @@
+'use strict';
+const app = {
+    setup: {
+        estudiantes: undefined,
+    }
+}
+
 function mostrar() {
     $("#estudiante").show();
     $("#send").on('click', function () {
